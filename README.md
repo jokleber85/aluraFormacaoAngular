1795f9b8445e2acb5bcc3dd3695b71e5669250d5
John Kleber <jo.kleber85@gmail.com>
2021-07-06 14:46:36 -0300

    Curso concluído

--- README.md
+++ README.md
@@ -4,3 +4,14 @@ Formação Angular</br>
 
 Angular e RxJS: Programação reativa (06 horas)
 ## <br />
+
+Instalar dependências na pasta (byteBankBroker/api) e (byteBankBroker)
+```js
+npm install
+npm start // byteBankBroker/api
+npm npm install -g @angular // byteBankBroker
+ng serve --open // byteBankBroker
+```
+</br>
+
+[Documentação da biblioteca RxJS](https://rxjs-dev.firebaseapp.com/guide/overview):
--- byteBankBroker/src/app/acoes/acoes.component.ts
+++ byteBankBroker/src/app/acoes/acoes.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { merge } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
 
+// 0import { Acoes } from './modelo/acoes';
+import { AcoesService } from './acoes.service';
+// import { Subscription } from 'rxjs';
+
+const ESPERA_DIGITACAO = 300;
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent {
+export class AcoesComponent { // implements OnInit, OnDestroy
   acoesInput = new FormControl();
 
-  constructor() {}
+  todaAcoes$ = this.acoesService.getAcoes().pipe(
+    tap(() => {console.log('Fluxo Inicial'); }));
+
+  filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+    debounceTime(ESPERA_DIGITACAO),
+    tap(() => {console.log('Fluxo do Filtro'); }),
+    tap(console.log),
+    filter((valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length),
+    distinctUntilChanged(),
+    switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado))
+  );
+
+  acoes$ = merge(this.todaAcoes$, this.filtroPeloInput$);
+
+/*acoes$ = this.acoesInput.valueChanges
+    .pipe(
+      tap(console.log),
+      switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
+      tap(console.log)
+    ); // this.acoesService.getAcoes(); /*acoes: Acoes; private subscription: Subscription; */
+
+  constructor(private acoesService: AcoesService) { }
+
+/*ngOnInit(){
+    this.subscription = this.acoesService.getAcoes()
+      .subscribe((acoes) => {
+        this.acoes = acoes;
+      });
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  } */
 }
